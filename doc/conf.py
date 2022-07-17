df7c74d647cadc5fd734593c6bd6a1bb106edc97
Stefan Ulbrich <6009224+StefanUlbrich@users.noreply.github.com>
2022-07-17 22:52:59 +0200

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Stefan Ulbrich'
 author = 'Stefan Ulbrich'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
